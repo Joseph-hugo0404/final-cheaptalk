379e9454109486c5533f5ef2ef6792adbfcaf472
Joseph Hugo <josephjhugo@gmail.com>
2022-12-27 15:53:45 +0800

    changeed

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -3,7 +3,7 @@
       <div class="sidebar-header">
           <div class="d-flex justify-content-between">
               <div class="logo">
-                  {{-- <a href="{{'/dashboard' }}"><img src="{{ URL::to('assets/images/logo/logo.png') }}" alt="Logo" srcset=""></a> --}}
+                  <h1>Final <br> Cheaptalk</h1>
               </div>
               <div class="toggler">
                   <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
